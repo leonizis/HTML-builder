580d25e4185e6ea807ecc27f9a14039f81f3cee1
Leonid <leonidu36@gmail.com>
2023-05-09 09:57:48 +0300

    fix: test 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,41 +1,35 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const dirPath = path.join(__dirname, 'secret-folder');
 
-// читаем содержимое директории асинхронно
-fs.readdir(dirPath, (err, files) => {
-  if (err) {
-    console.error(`Ошибка чтения директории: ${err}`);
-    return;
-  }
-
-  // фильтруем только файлы
-  const promises = files.map(fileName => {
-    const filePath = path.join(dirPath, fileName);
-    return new Promise((resolve, reject) => {
-      fs.stat(filePath, (err, stats) => {
-        if (err) {
-          console.error(`Ошибка чтения файла: ${err}`);
-          resolve(false);
-        }
-        resolve(stats.isFile() && path.extname(filePath) !== '');
-      });
+fs.readdir(dirPath)
+  .then(files => {
+    // фильтруем только файлы
+    const promises = files.map(fileName => {
+      const filePath = path.join(dirPath, fileName);
+      return fs.stat(filePath)
+        .then(stats => stats.isFile() && path.extname(filePath) !== '');
     });
-  });
 
-  Promise.all(promises).then(results => {
-    const filteredFiles = files.filter((fileName, index) => results[index]);
-
+    return Promise.all(promises)
+      .then(results => files.filter((fileName, index) => results[index]));
+  })
+  .then(filteredFiles => {
     // выводим информацию о каждом файле
-    filteredFiles.forEach(fileName => {
+    filteredFiles.forEach(async fileName => {
       const filePath = path.join(dirPath, fileName);
       const fileExt = path.extname(filePath).slice(1);
-      const fileSizeInBytes = fs.statSync(filePath).size;
-      const fileSizeInKb = fileSizeInBytes / 1024;
-      console.log(`${path.basename(filePath, path.extname(filePath))} - ${fileExt} - ${fileSizeInKb} Kb`);
+      try {
+        const stats = await fs.stat(filePath);
+        const fileSizeInBytes = stats.size;
+        const fileSizeInKb = fileSizeInBytes / 1024;
+        console.log(`${path.basename(filePath, path.extname(filePath))} - ${fileExt} - ${fileSizeInKb} Kb`);
+      } catch (err) {
+        console.error(`Ошибка чтения файла: ${err}`);
+      }
     });
-  }).catch(err => {
-    console.error(`Ошибка при фильтрации файлов: ${err}`);
+  })
+  .catch(err => {
+    console.error(`Ошибка чтения директории: ${err}`);
   });
-});
