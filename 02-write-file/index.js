2785eb84f19c6b75c019618ecf63c7bfa351e10c
Leonid <leonidu36@gmail.com>
2023-05-09 10:02:10 +0300

    fix: test 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,32 +9,15 @@ const rl = readline.createInterface({
 const filePath = 'text.txt';
 
 // проверяем, существует ли файл
-let exists = false;
-try {
-  fs.accessSync(`./02-write-file/${filePath}`, fs.constants.F_OK);
-  exists = true;
-} catch (err) {}
-
-if (!exists) {
-  // создаем файл, если его нет
-  fs.writeFile(`./02-write-file/${filePath}`, '', err => {
-    if (err) {
-      console.error(err);
-    }
+fs.promises.access(`./02-write-file/${filePath}`, fs.constants.F_OK)
+  .catch(() => {
+    // создаем файл, если его нет
+    return fs.promises.writeFile(`./02-write-file/${filePath}`, '');
   });
-}
 
 // функция для записи данных в файл
 function appendToFile(data) {
-  return new Promise((resolve, reject) => {
-    fs.appendFile(`./02-write-file/${filePath}`, data, err => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+  return fs.promises.appendFile(`./02-write-file/${filePath}`, data);
 }
 
 // выводим приглашение на ввод текста
@@ -42,16 +25,20 @@ rl.setPrompt('Введите текст: ');
 rl.prompt();
 
 // обрабатываем ввод текста
-rl.on('line', async (input) => {
+rl.on('line', (input) => {
   if (input.toLowerCase() === 'exit') {
     // если пользователь ввел "exit", завершаем работу программы
     console.log('До свидания!');
     process.exit(0);
   } else {
     // записываем данные в файл
-    await appendToFile(`${input}\n`);
-    // выводим приглашение на ввод текста
-    rl.prompt();
+    appendToFile(`${input}\n`).then(() => {
+      // выводим приглашение на ввод текста
+      rl.prompt();
+    }).catch(err => {
+      console.error(err);
+      rl.close();
+    });
   }
 });
 
